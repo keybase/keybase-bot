dfbce1a672e5348d63657739559ab2d7003d2ec3
Nathan Smith <nathan@nathansmith.io>
2018-10-19 13:12:15 -0700

    Update hello_world

--- demos/hello_world.js
+++ demos/hello_world.js
@@ -1,34 +1,44 @@
 #!/usr/bin/env node
-var Bot = require('../index.js').Bot
+const {Bot} = require('../index.js')
 
-var bot = new Bot()
+const bot = new Bot()
 
-bot.init({verbose: false}, function (err) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
+bot.init(
+  {
+    username: process.env.KB_USERNAME,
+    paperkey: process.env.KB_PAPERKEY,
+    verbose: false,
+  },
+  err => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
 
-    var channel = {
-      name: 'kbot,' + bot.myInfo().username,
-      public: false,
-      topic_type: 'chat',
+      // var channel = {
+      //   name: 'kbot,' + bot.myInfo().username,
+      //   public: false,
+      //   topic_type: 'chat',
+      // }
+
+      // var sendArg = {
+      //   channel: channel,
+      //   message: {
+      //     body:
+      //       'Hello kbot! This is ' +
+      //       bot.myInfo().username +
+      //       ' saying hello from my device ' +
+      //       bot.myInfo().devicename,
+      //   },
+      // }
+
+      // bot.chatSend(sendArg, function (err) {
+      //   if (err) {
+      //     console.log(err)
+      //   } else {
+      //     console.log('Message sent!')
+      //   }
+      // })
     }
-
-    var sendArg = {
-      channel: channel,
-      message: {
-        body: 'Hello kbot! This is ' + bot.myInfo().username + ' saying hello from my device ' + bot.myInfo().devicename,
-      },
-    }
-
-    bot.chatSend(sendArg, function (err) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log('Message sent!')
-      }
-    });
   }
-
-});
+)
