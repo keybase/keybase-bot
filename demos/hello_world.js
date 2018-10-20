feab04a690c95bb6e1aff02b1bb791aaf6931f24
Nathan Smith <nathan@nathansmith.io>
2018-10-20 13:18:20 -0700

    hello_world works

--- demos/hello_world.js
+++ demos/hello_world.js
@@ -15,30 +15,30 @@ bot.init(
     } else {
       console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
 
-      // var channel = {
-      //   name: 'kbot,' + bot.myInfo().username,
-      //   public: false,
-      //   topic_type: 'chat',
-      // }
+      var channel = {
+        name: 'kbot,' + bot.myInfo().username,
+        public: false,
+        topic_type: 'chat',
+      }
 
-      // var sendArg = {
-      //   channel: channel,
-      //   message: {
-      //     body:
-      //       'Hello kbot! This is ' +
-      //       bot.myInfo().username +
-      //       ' saying hello from my device ' +
-      //       bot.myInfo().devicename,
-      //   },
-      // }
+      var sendArg = {
+        channel: channel,
+        message: {
+          body:
+            'Hello kbot! This is ' +
+            bot.myInfo().username +
+            ' saying hello from my device ' +
+            bot.myInfo().devicename,
+        },
+      }
 
-      // bot.chatSend(sendArg, function (err) {
-      //   if (err) {
-      //     console.log(err)
-      //   } else {
-      //     console.log('Message sent!')
-      //   }
-      // })
+      bot.chatSend(sendArg, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('Message sent!')
+        }
+      })
     }
   }
 )
