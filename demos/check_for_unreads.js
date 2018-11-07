1b23bde43abadf99279151f433e36a74d86a0c4f
Jacob Young <jacob@jry.io>
2018-11-06 17:46:16 -0800

    Update flow typing

--- demos/check_for_unreads.js
+++ demos/check_for_unreads.js
@@ -10,8 +10,12 @@ bot
     verbose: false,
   })
   .then(() => {
-    bot.chatList(null).then(res => {
-      const unreadCount = res.conversations.filter(c => c.unread).length
-      console.log(`You have ${unreadCount} unread conversations out of ${res.conversations.length} total`)
-    })
+    bot
+      .chatList(null)
+      .then(res => {
+        const unreadCount = res.conversations.filter(c => c.unread).length
+        console.log(`You have ${unreadCount} unread conversations out of ${res.conversations.length} total`)
+      })
+      .catch(err => console.log(err))
   })
+  .catch(err => console.log('Failed to initialize', err))
