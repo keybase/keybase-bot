0b1e61264ec4947c8a631283c79b73bcb077639f
Jacob Young <jacob@jry.io>
2018-11-11 11:09:04 -0800

    Update demo to process STDIN for paperkey

--- demos/hello_world.js
+++ demos/hello_world.js
@@ -3,37 +3,58 @@ const {Bot} = require('../index.js')
 
 const bot = new Bot()
 
-bot
-  .init({
-    username: process.env.KB_USERNAME,
-    paperkey: process.env.KB_PAPERKEY,
-    verbose: false,
+const readStdin = () => {
+  return new Promise((resolve, reject) => {
+    const stdin = []
+    let buf = null
+    process.stdin.on('data', chunk => {
+      stdin.push(chunk)
+    })
+
+    process.stdin.on('end', () => {
+      buf = Buffer.concat(stdin)
+      resolve(buf)
+    })
+
+    process.stdin.on('error', err => {
+      reject(err)
+    })
   })
-  .then(() => {
-    console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
+}
 
-    const channel = {
-      name: 'kbot,' + bot.myInfo().username,
-      public: false,
-      topic_type: 'chat',
-    }
+readStdin().then(stdin => {
+  bot
+    .init({
+      username: process.env.KB_USERNAME,
+      paperkey: stdin,
+      verbose: false,
+    })
+    .then(() => {
+      console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
 
-    const sendArg = {
-      channel: channel,
-      message: {
-        body:
-          'Hello kbot! This is ' +
-          bot.myInfo().username +
-          ' saying hello from my device ' +
-          bot.myInfo().devicename,
-      },
-    }
+      const channel = {
+        name: 'kbot,' + bot.myInfo().username,
+        public: false,
+        topic_type: 'chat',
+      }
 
-    bot
-      .chatSend(sendArg)
-      .then(() => {
-        console.log('Message sent!')
-      })
-      .catch(err => console.log('Message failed to send', err))
-  })
-  .catch(err => console.log(err))
+      const sendArg = {
+        channel: channel,
+        message: {
+          body:
+            'Hello kbot! This is ' +
+            bot.myInfo().username +
+            ' saying hello from my device ' +
+            bot.myInfo().devicename,
+        },
+      }
+
+      bot
+        .chatSend(sendArg)
+        .then(() => {
+          console.log('Message sent!')
+        })
+        .catch(err => console.log('Message failed to send', err))
+    })
+    .catch(err => console.log(err))
+})
