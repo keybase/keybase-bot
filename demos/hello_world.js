b31e025894ddd7ed54a8daa46a0dde7bcb61bb28
Jacob Young <jacob@jry.io>
2018-11-13 18:14:38 -0800

    Revert "Update demo to process STDIN for paperkey"
    This reverts commit 0b1e61264ec4947c8a631283c79b73bcb077639f.

--- demos/hello_world.js
+++ demos/hello_world.js
@@ -3,58 +3,37 @@ const {Bot} = require('../index.js')
 
 const bot = new Bot()
 
-const readStdin = () => {
-  return new Promise((resolve, reject) => {
-    const stdin = []
-    let buf = null
-    process.stdin.on('data', chunk => {
-      stdin.push(chunk)
-    })
-
-    process.stdin.on('end', () => {
-      buf = Buffer.concat(stdin)
-      resolve(buf)
-    })
-
-    process.stdin.on('error', err => {
-      reject(err)
-    })
+bot
+  .init({
+    username: process.env.KB_USERNAME,
+    paperkey: process.env.KB_PAPERKEY,
+    verbose: false,
   })
-}
+  .then(() => {
+    console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
 
-readStdin().then(stdin => {
-  bot
-    .init({
-      username: process.env.KB_USERNAME,
-      paperkey: stdin,
-      verbose: false,
-    })
-    .then(() => {
-      console.log('Your bot is initialized. It is logged in as ' + bot.myInfo().username)
+    const channel = {
+      name: 'kbot,' + bot.myInfo().username,
+      public: false,
+      topic_type: 'chat',
+    }
 
-      const channel = {
-        name: 'kbot,' + bot.myInfo().username,
-        public: false,
-        topic_type: 'chat',
-      }
+    const sendArg = {
+      channel: channel,
+      message: {
+        body:
+          'Hello kbot! This is ' +
+          bot.myInfo().username +
+          ' saying hello from my device ' +
+          bot.myInfo().devicename,
+      },
+    }
 
-      const sendArg = {
-        channel: channel,
-        message: {
-          body:
-            'Hello kbot! This is ' +
-            bot.myInfo().username +
-            ' saying hello from my device ' +
-            bot.myInfo().devicename,
-        },
-      }
-
-      bot
-        .chatSend(sendArg)
-        .then(() => {
-          console.log('Message sent!')
-        })
-        .catch(err => console.log('Message failed to send', err))
-    })
-    .catch(err => console.log(err))
-})
+    bot
+      .chatSend(sendArg)
+      .then(() => {
+        console.log('Message sent!')
+      })
+      .catch(err => console.log('Message failed to send', err))
+  })
+  .catch(err => console.log(err))
