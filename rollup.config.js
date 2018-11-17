167beb65c92f42ebebd046bcb8bfc0c937f6cceb
Nathan Smith <nathan@nathansmith.io>
2018-11-17 15:10:23 -0800

    Update package.json

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,14 @@ import babel from 'rollup-plugin-babel'
 export default {
   input: 'lib/entry.js',
   output: {
-    file: 'bundle.js',
+    file: 'index.js',
     format: 'cjs',
+    sourcemap: true,
   },
   plugins: [
     babel({
       exclude: 'node_modules/**',
     }),
   ],
+  external: ['child_process', 'readline'],
 }
