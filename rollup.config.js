f3927211309591df4750920ff4dc2dec1eb10744
Nathan Smith <nathan@nathansmith.io>
2018-11-19 09:28:28 -0800

    Use Rollup, and merge into master (#37)
    * Initial stuff
    
    * woo
    
    * Remove some unused files
    
    * add watchChannelForNewMessages
    
    * No need for utils
    
    * Math bot copy -> math bot
    
    * Add some documentation
    
    * No copy in launch.json
    
    * remove some test stuff in math_bot
    
    * Update flow typings
    
    * Better error handling
    
    * es6 is msgReply
    
    * Use readline
    
    * rl -> lineReader
    
    * add rollup stuff
    
    * Update package.json
    
    * Update ignore files
    
    * Remvoe webpack stuff
    
    * * -> map
    
    * Generate API Documentation With Flow Types (#34)
    
    * Move contributions section to the end
    
    * Remove commands section from READM
    
    * Add table of contents
    
    * Update with README from nathan/api-listen
    
    * Updates to methods from nathan/api-listen
    
    * Update types and remove unused types
    
    * Change option signatures to camelCase
    
    * Remove testing file and indent documentation.yml
    
    * Fix syntax error
    
    * add rollup stuff
    
    * Update package.json
    
    * Update ignore files
    
    * Remvoe webpack stuff
    
    * * -> map
    
    * Remove incorrect package after rebase
    
    * remove some unused eslint stuff

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'lib/entry.js',
+  output: {
+    file: 'index.js',
+    format: 'cjs',
+    sourcemap: true,
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+  external: ['child_process', 'readline'],
+}
