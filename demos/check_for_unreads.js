a63e8939602424d7f12e00f8fe7061f8a7ea0e6d
Nathan Smith <nathan@nathansmith.io>
2018-11-24 00:05:56 -0800

    Add null check

--- demos/check_for_unreads.js
+++ demos/check_for_unreads.js
@@ -8,9 +8,14 @@ async function main() {
     await bot.init({username: process.env.KB_USERNAME, paperkey: process.env.KB_PAPERKEY, verbose: false})
     const res = await bot.chatList({
       showErrors: true,
+      unreadOnly: true,
     })
-    const unreadCount = res.conversations.filter(c => c.unread).length
-    console.log(`You have ${unreadCount} unread conversations out of ${res.conversations.length} total`)
+    if (res) {
+      const unreadCount = res.conversations.filter(c => c.unread).length
+      console.log(`You have ${unreadCount} unread conversations out of ${res.conversations.length} total`)
+    } else {
+      console.log('You have no messages. Go chat with someone!')
+    }
   } catch (error) {
     console.error(error)
   } finally {
