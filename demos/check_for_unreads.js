a2f08a29b189cc372f98e4051433138de873a267
Jacob Young <jacob@jry.io>
2018-11-29 00:53:23 -0800

    Revert check_for_unreads to master version

--- demos/check_for_unreads.js
+++ demos/check_for_unreads.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-const Bot = require('../index.js')
+const {Bot} = require('../index.js')
 
 async function main() {
   const bot = new Bot()
+
   try {
     await bot.init({username: process.env.KB_USERNAME, paperkey: process.env.KB_PAPERKEY, verbose: false})
-    const conversations = await bot.chat.list({showErrors: true, unreadOnly: false})
-
-    console.log(
-      `You have ${conversations.filter(c => c.unread).length} unread conversations out of ${
-        conversations.length
-      } total`
-    )
+    const res = await bot.chatList({
+      showErrors: true,
+      unreadOnly: true,
+    })
+    const unreadCount = res.conversations.filter(c => c.unread).length
+    console.log(`You have ${unreadCount} unread conversations out of ${res.conversations.length} total`)
   } catch (error) {
     console.error(error)
   } finally {
