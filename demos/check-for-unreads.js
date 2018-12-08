d19bc5da4be0a348f879286c7a11cc7e10fb4c2c
Jacob Young <jacob@jry.io>
2018-12-07 19:54:16 -0800

    Downgrade Demos to Promise API (#53)
    * Downgrade existing demos and move others to /es7
    
    * Match filenames
    
    * Update demos/math-bot.js

---
+++ demos/check-for-unreads.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const Bot = require('../index.js')
+
+function main() {
+  const bot = new Bot()
+  const username = process.env.KB_USERNAME
+  const paperkey = process.env.KB_PAPERKEY
+  bot
+    .init(username, paperkey)
+    .then(() => {
+      bot.chat.list({showErrors: true}).then(conversations => {
+        if (conversations.length) {
+          const unreadCount = conversations.filter(c => c.unread).length
+          console.log(`You have ${unreadCount} unread conversations out of ${conversations.length} total`)
+        } else {
+          console.log('You have no messages. Go chat with someone!')
+        }
+        bot.deinit()
+      })
+    })
+    .catch(error => {
+      console.error(error)
+      bot.deinit()
+    })
+}
+
+main()
---
+++ demos/hello-world.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const Bot = require('../index.js')
+
+function main() {
+  const bot = new Bot()
+  const username = process.env.KB_USERNAME
+  const paperkey = process.env.KB_PAPERKEY
+  bot
+    .init(username, paperkey, {verbose: false})
+    .then(() => {
+      console.log(`Your bot is initialized. It is logged in as ${bot.myInfo().username}`)
+      const channel = {name: 'kbot,' + bot.myInfo().username, public: false, topic_type: 'chat'}
+      const message = {
+        body: `Hello kbot! This is ${bot.myInfo().username} saying hello from my device ${
+          bot.myInfo().devicename
+        }`,
+      }
+      bot.chat
+        .send(channel, message)
+        .then(() => {
+          console.log('Message sent!')
+          bot.deinit()
+        })
+        .catch(error => {
+          console.error(error)
+          bot.deinit()
+        })
+    })
+    .catch(error => {
+      console.error(error)
+      bot.deinit()
+    })
+}
+
+main()
