75b4ee94ed048a66e4cf7cc85a99f85bd362de3e
Nathan Smith <nathan@nathansmith.io>
2018-12-13 16:30:44 -0800

    Some testing

--- __tests__/alice_init.test.js
+++ __tests__/alice_init.test.js
@@ -1,17 +1,21 @@
-#!/usr/bin/env node
-/* eslint-env jest */
+import Bot from '../lib'
+import config from './tests.config.js'
 
-const Bot = require('../index.js')
-const config = require('./tests.config.js')
-const alice = new Bot()
-
-async function runTest() {
-  it(`alice can init()`, async () => {
-    jest.setTimeout(10000)
+describe('Keybase Bot', () => {
+  it('Can init and deinit', async () => {
+    const alice = new Bot()
     await alice.init(config.alice1.username, config.alice1.paperkey)
     expect(alice.myInfo().username).toBe(config.alice1.username)
+    expect(alice.myInfo().devicename).toBe(
+      config.alice1.paperkey
+        .split(' ')
+        .slice(0, 2)
+        .join(' ')
+    )
     await alice.deinit()
   })
-}
 
-runTest()
+  it('Can init and deinit with a custom home directory', async () => {})
+
+  it('Can init and deinit with an existing Keybase service', async () => {})
+})
