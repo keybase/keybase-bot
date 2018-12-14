470ef6d0db276e110e1c3efc08f32d346ce2e1df
Nathan Smith <nathan@nathansmith.io>
2018-12-13 16:56:07 -0800

    entry -> index.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  input: 'lib/entry.js',
+  input: 'lib/index.js',
   output: {
     file: 'index.js',
     format: 'cjs',
