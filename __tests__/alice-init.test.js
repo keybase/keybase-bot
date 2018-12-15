c06f0659aa99c7f7352b96214037041c5db521ce
Nathan Smith <nathan@nathansmith.io>
2018-12-15 12:01:38 -0800

    test env for *.test.js files (#76)

--- __tests__/alice-init.test.js
+++ __tests__/alice-init.test.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-env jest */
 import Bot from '../lib'
 import config from './tests.config.js'
 
--- __tests__/shutdown.test.js
+++ __tests__/shutdown.test.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-env jest */
 
 import fs from 'fs'
 import {promisify} from 'util'
