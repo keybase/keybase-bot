29046bffa0d72cad6850b916d4306cde71368753
Nathan Smith <nathan@nathansmith.io>
2018-12-15 20:28:59 -0800

    Use <rootDir>

--- jest.config.js
+++ jest.config.js
@@ -118,7 +118,7 @@ module.exports = {
   // setupFiles: [],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  setupTestFrameworkScriptFile: '__tests__/setup.js',
+  setupTestFrameworkScriptFile: '<rootDir>/__tests__/setup.js',
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
