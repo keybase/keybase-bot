a72d70f515a638d9da3bc597d0f08ae0ce28cd4c
Nathan Smith <nathan@nathansmith.io>
2018-12-20 18:47:55 -0800

    initFromRunningService

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -1,5 +1,6 @@
 import Bot from '../lib'
 import config from './tests.config.js'
+import {keybaseServiceStartup, keybaseExec, randomTempDir} from '../lib/utils'
 
 describe('Keybase bot initialization', () => {
   it('can init with a username and paperkey', async () => {
@@ -19,10 +20,24 @@ describe('Keybase bot initialization', () => {
     await expect(alice.init(config.bots.alice1.username, undefined)).rejects.toThrowError()
   })
 
-  // it('can init if a service with a logged in user is currently running', async () => {
-  //   const alice = new Bot()
-  //   await alice.init(config.bots.alice1.username, config.bots.alice1.paperkey)
-  //   expect(alice.myInfo().username).toBe(config.bots.alice1.username)
-  //   await alice.deinit()
-  // })
+  it('can init if a service with a logged in user is currently running', async () => {
+    const homeDir = randomTempDir()
+    await keybaseServiceStartup(homeDir)
+    // Ideally, this should use `login` instead of `oneshot` but `login` doesn't provide a programmatic way to input a password.
+    await keybaseExec(homeDir, ['oneshot', '--username', config.bots.alice1.username], {
+      stdinBuffer: config.bots.alice1.paperkey,
+    })
+
+    const alice = new Bot()
+    await alice.initFromRunningService(homeDir)
+
+    expect(alice.myInfo().username).toBe(config.bots.alice1.username)
+    await alice.deinit()
+  })
+
+  it('throws an error if an invalid home directory is given as the location of a currently running service', async () => {
+    const homeDir = '/blah/fake-dir-that-does-not-exist'
+    const alice = new Bot()
+    await expect(alice.initFromRunningService(homeDir)).rejects.toThrowError()
+  })
 })
