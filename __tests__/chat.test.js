7ff8eb71baa78401304c14ae1fadf9ea97379dab
Nathan Smith <nathan@nathansmith.io>
2018-12-27 16:53:19 -0800

    Chat delete

--- __tests__/chat.test.js
+++ __tests__/chat.test.js
@@ -40,16 +40,42 @@ describe('Chat read', () => {
   })
 })
 
-// describe('Chat send', () => {
-//   it('Sends a message to a certain channel and returns an empty promise', async () => {
-//     const alice = new Bot()
-//     await alice.init(config.bots.alice1.username, config.bots.alice1.paperkey)
+describe('Chat send', () => {
+  it('Sends a message to a certain channel and returns an empty promise', async () => {
+    const alice = new Bot()
+    await alice.init(config.bots.alice1.username, config.bots.alice1.paperkey)
 
-//     const channel = {name: `${config.bots.alice1.username},${config.bots.bob1.username}`}
-//     const message = {body: 'Testing chat.send()!'}
-//     await alice.chat.send(channel, message)
+    const channel = {name: `${config.bots.alice1.username},${config.bots.bob1.username}`}
+    const message = {body: 'Testing chat.send()!'}
+    await alice.chat.send(channel, message)
 
-//     const messages = await alice.chat.read(channel, {peek: true}
-//     expect(messages[0]).
-//   })
-// })
+    const messages = await alice.chat.read(channel, {peek: true})
+    expect(messages[0].sender.username).toEqual(alice.myInfo().username)
+    expect(messages[0].content.text.body).toEqual(message.body)
+  })
+})
+
+describe('Chat delet', () => {
+  it('Deletes a message to a certain channel and returns an empty promise', async () => {
+    const alice = new Bot()
+    await alice.init(config.bots.alice1.username, config.bots.alice1.paperkey)
+
+    const channel = {name: `${config.bots.alice1.username},${config.bots.bob1.username}`}
+    const message = {body: 'Testing chat.send()!'}
+    await alice.chat.send(channel, message)
+
+    const messages = await alice.chat.read(channel, {peek: true})
+    expect(messages[0].sender.username).toEqual(alice.myInfo().username)
+    expect(messages[0].content.text.body).toEqual(message.body)
+
+    const {id} = messages[0]
+    await alice.chat.delete(channel, id)
+
+    const newMessages = await alice.chat.read(channel, {peek: true})
+    expect(newMessages[0].id).toEqual(id + 1)
+    expect(newMessages[0].content.delete.messageIDs).toContain(id)
+    expect(newMessages[0].content.delete.messageIDs).toHaveLength(1)
+    expect(newMessages[1].id).toEqual(id - 1)
+    await alice.deinit()
+  })
+})
