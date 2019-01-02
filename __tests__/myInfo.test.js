d4319ff1a2c4c3a91f7c3c8a56da3ea7ee9a0079
Nathan Smith <nathan@nathansmith.io>
2019-01-02 08:19:52 -0800

    Type myInfo.test

--- __tests__/myInfo.test.js
+++ __tests__/myInfo.test.js
@@ -1,13 +1,18 @@
+// @flow
 import Bot from '../lib'
 import config from './tests.config.js'
 import {publicPaperkeyLabel} from './test-utils'
 
-describe('bot.myInfo()', async () => {
+describe('bot.myInfo()', () => {
   it('returns a username, devicename, and homeDir when the bot is initialized', async () => {
     const alice = new Bot()
     await alice.init(config.bots.alice1.username, config.bots.alice1.paperkey)
     const aliceInfo = alice.myInfo()
+    expect(aliceInfo).not.toBeNull()
+    // We don't care about the possibility that aliceInfo could be null since that will error which we want
+    // $FlowFixMe
     expect(aliceInfo.username).toBe(config.bots.alice1.username)
+    // $FlowFixMe
     expect(aliceInfo.devicename).toBe(publicPaperkeyLabel(config.bots.alice1.paperkey))
     expect(aliceInfo).toHaveProperty('homeDir')
     await alice.deinit()
