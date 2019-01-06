c68ac35fa2c0d615a4e78b37667375ec27328953
Nathan Smith <nathan@nathansmith.io>
2019-01-06 10:42:28 -0800

    Stub out tests

--- __tests__/wallet.test.js
+++ __tests__/wallet.test.js
@@ -35,11 +35,30 @@ describe('Wallet Methods', () => {
   })
 
   describe('Wallet history', () => {
-    it('Lists all the transactions of an account', async () => {
-      const accounts = await charlie1.wallet.balances()
-      const transactions = await charlie1.wallet.history(accounts[0].accountId)
-      expect(Array.isArray(transactions)).toBe(true)
-      console.log(transactions)
-    })
+    it('Provides the details of all transactions involving the specified account', () => {})
+    it('Throws an error if the accountId is invalid', () => {})
+  })
+
+  describe('Wallet details', () => {
+    it('Provides the details of a transaction by its id', () => {})
+    it('Throws an error if the transaction id is invalid', () => {})
+  })
+
+  describe('Wallet lookup', () => {
+    it("Returns a user's account id and Keybase username upon successful lookup", () => {})
+    it('Throws an error if a user has not set up their wallet', () => {})
+    it('Throws an error if a user cannot be found', () => {})
+  })
+
+  describe('Wallet send', () => {
+    it('Sends money', () => {})
+    it('Throws an error if given an invalid recipient', () => {})
+    it('Throws an error if given an invalid amount')
+    it('Throws an error if given an invalid currency')
+  })
+
+  describe('Wallet cancel', () => {
+    it('Successfully cancels transactions if to a user who has not set up their account', () => {})
+    it('Throws an error if it is given a transaction that cannot be canceled', () => {})
   })
 })
