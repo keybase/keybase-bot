ad0ed7172e859698870036d3a193c75951dd90e2
Chris Coyne <ccoyne77@gmail.com>
2019-03-07 12:02:06 -0500

    wip

--- __tests__/team.test.js
+++ __tests__/team.test.js
@@ -18,13 +18,13 @@ describe('Team Methods', () => {
 
   beforeAll(async () => {
     await alice1.init(config.bots.alice1.username, config.bots.alice1.paperkey)
-    //await alice2.init(config.bots.alice2.username, config.bots.alice2.paperkey)
-    //await bob.init(config.bots.bob1.username, config.bots.bob1.paperkey)
+    // await alice2.init(config.bots.alice2.username, config.bots.alice2.paperkey)
+    // await bob.init(config.bots.bob1.username, config.bots.bob1.paperkey)
   })
   afterAll(async () => {
     await alice1.deinit()
-    //await alice2.deinit()
-    //await bob.deinit()
+    // await alice2.deinit()
+    // await bob.deinit()
   })
 
   describe('Team list', () => {
