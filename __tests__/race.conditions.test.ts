f86242b253f5a06383e513c4b13c0354ea63af14
Chris Coyne <ccoyne77@gmail.com>
2019-11-07 12:14:37 -0500

    10 in test

--- __tests__/race.conditions.test.ts
+++ __tests__/race.conditions.test.ts
@@ -2,18 +2,21 @@ import Bot from '../lib'
 import config from './tests.config'
 
 describe('init/chat/deinit checking', (): void => {
-  it('can init, send, deinit', async (): Promise<void> => {
-    const body = `race-${Math.floor(Math.random() * 1000000000)}`
+  const loop = 10
+  it(`can init, send ${loop}, deinit`, async (): Promise<void> => {
+    const code = Date.now()
     const alice = new Bot()
     const bob = new Bot()
     const channel = {name: `${config.bots.alice1.username},${config.bots.bob1.username}`}
     await alice.init(config.bots.alice1.username, config.bots.alice1.paperkey)
-    await alice.chat.send(channel, {body})
+    for (let i = 1; i <= loop; i++) {
+      await alice.chat.send(channel, {body: `race-${i}-${code}`})
+    }
     await alice.deinit()
     await bob.init(config.bots.bob1.username, config.bots.bob1.paperkey)
     await bob.chat.read({name: config.bots.alice1.username})
     const read1 = await bob.chat.read(channel, {pagination: {num: 1}})
-    expect(read1.messages[0].content.text.body).toContain(body)
+    expect(read1.messages[0].content.text.body).toContain(`race-${loop}-${code}`)
     await bob.deinit()
   })
 })
